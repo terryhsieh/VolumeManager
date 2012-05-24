07f0d9e59c08df009d5e195c2a071293b765d3a9
terry.hsieh <terry.hsieh@delta.com.tw>
2012-05-24 17:56:35 +0800

    modify export and bgcommander

--- BootVolumeUtil.py
+++ BootVolumeUtil.py
@@ -5,12 +5,10 @@ Utility functions for bootserver to create client images and volumes.
 import os
 from shutil import copytree, rmtree
 
-StorageMountPoint="/templateVol"
-ExportPath="192.168.100.10:/nfsroot_test"
-my_ip = '192.168.100.101'
+bootServerIp = '192.168.100.101'
 
-
-#check if mount point exist
+templateStor = "/templateVol"
+exportRootPath = bootServerIp+":/templateVol"
 
 deployDictSample = {	'zone_id':'zone001',
 			'hostname':['host1','host2'],
@@ -18,54 +16,38 @@ deployDictSample = {	'zone_id':'zone001',
 			'kernel':'vmlinuz-3.2.0-23-generic',
 			'ramdisk':'initrd.img-3.2.0-23-generic',
 			'volume':'vol_openstack',
-			'volume_readonly':True}
+			'volume_readonly':False}
 
-if ( not os.path.ismount(StorageMountPoint)):
-	print("The mount point does not exist!, mount it~")
-	cmd=" sudo mount -t nfs " + ExportPath + " " + StorageMountPoint
-	os.system(cmd)
+# check if /templateStor exist 
+if ( not os.path.isdir(templateStor)):
+	print("The tempate storage folder does not exist!, check if the iSCSI connection to backend storage correctly")
 
 def boot_up_by_images(deployDict):
-	print(deployDict)
+	
+	if ( os.path.isdir(templateStor + '/' + deployDict.get('zone_id'))):
+		print('The zone id: ' + deployDict.get('zone_id') + ' already existed in the template Storage, delete it.')
+		rmtree(templateStor + '/' + deployDict.get('zone_id'))
+	
 	if deployDict.get("volume_readonly") == True:
 		print('volume_readonly is True')
-		if ( os.path.isdir(StorageMountPoint+'/'+deployDict.get('zone_id'))):
-			print('zone already existed, delete it.')
-			rmtree(StorageMountPoint+'/'+deployDict.get('zone_id'))
 			
-			source_dir = StorageMountPoint+'/'+deployDict.get('volume')
-			dest_dir = StorageMountPoint+'/'+deployDict.get('zone_id')
-			copytree(source_dir,dest_dir)
-			
-			modify_exports_file(deployDict.get('zone_id'))
-			call_bgcommander(deployDict)
-		else:
-			print('copy...')
-			source_dir = StorageMountPoint+'/'+deployDict.get('volume')
-                        dest_dir = StorageMountPoint+'/'+deployDict.get('zone_id')
-                        copytree(source_dir,dest_dir)
-			
-			modify_exports_file(deployDict.get('zone_id'))	
-			call_bgcommander(deployDict)
+		source_dir = templateStor+'/'+deployDict.get('volume')
+		dest_dir = templateStor+'/'+deployDict.get('zone_id')
+		copytree(source_dir,dest_dir)
+		
+		modify_exports_file(deployDict)
+		call_bgcommander(deployDict)
+
 	else:
 		print('volume_readonly is False')
-		if ( os.path.isdir(StorageMountPoint+'/'+deployDict.get('zone_id'))):
-			print('zone already existed, delete it.')
-			rmtree(StorageMountPoint+'/'+deployDict.get('zone_id'))
-			
-			source_dir = StorageMountPoint+'/'+deployDict.get('volume')
-			for node in deployDict.get('hostname'):
-				dest_dir = StorageMountPoint+'/'+deployDict.get('zone_id') + '/nodes/' + node
-				copytree(source_dir, dest_dir)
 
-				modify_exports_file(deployDict.get('zone_id'))
-		else:
-			print('copy.....')
-			source_dir = StorageMountPoint+'/'+deployDict.get('volume')
-                        for node in deployDict.get('hostname'):
-                                dest_dir = StorageMountPoint+'/'+deployDict.get('zone_id') + '/nodes/' + node
-                                copytree(source_dir, dest_dir)
-				modify_exports_file(deployDict.get('zone_id'))
+		source_dir = templateStor+'/'+deployDict.get('volume')
+		for node in deployDict.get('hostname'):
+			dest_dir = templateStor+'/'+deployDict.get('zone_id') + '/nodes/' + node
+			copytree(source_dir, dest_dir)
+			modify_exports_file(deployDict)
+		call_bgcommander(deployDict)
+
 def reboot(hostname, addr):
 	return True
 
@@ -76,39 +58,39 @@ def listImages():
 	pass
 
 # The export entry based on zone_id, ex: one zone has one export entry 
-def modify_exports_file(zone_id):
-	print(zone_id)
+def modify_exports_file(deployDict):
 	myfile = open('/etc/exports','a+r')
-	entry = StorageMountPoint +'/' + zone_id +'\t' + '*(rw,async,no_root_squash,no_subtree_check)'+'\n'
-	if (myfile.read().find(zone_id) > 0):
-		print(zone_id + ' entry already existed in the exports file')
+	if (deployDict.get('volume_readonly') == True):
+		entry = templateStor +'/' + deployDict.get('zone_id') +'\t' + '*(ro,async,no_root_squash,no_subtree_check)'+'\n'
+	else:
+		entry = templateStor +'/' + deployDict.get('zone_id') +'\t' + '*(rw,async,no_root_squash,no_subtree_check)'+'\n'
+
+
+	if (myfile.read().find(deployDict.get('zone_id')) > 0):
+		print(' The zone id: ' + deployDict.get('zone_id') + ' already existed in the exports file')
 	else:
 		myfile.write(entry)
+
 	myfile.close()
-	
 	os.system('exportfs -rv')
 
 def call_bgcommander(deployDict):
-	if (deployDict.get('volume_readyonly') == True):
-		nfs_path = StorageMountPoint+'/'+deployDict.get('zone_id')
+	if (deployDict.get('volume_readonly') == True):
+		nfs_path = templateStor+'/'+deployDict.get('zone_id')
 		for t_addr in deployDict.get('addr'):
-			command = 'bgcommander ' + ip + ' ' + nfs_path + 'ro'
+			command = './bgcommander ' + ip + ' ' + nfs_path + 'ro'
 			print(command)
 #			os.system(command)
 	else:
-		nfs_path = StorageMountPoint+'/'+deployDict.get('zone_id')
+		nfs_path = templateStor+'/'+deployDict.get('zone_id')
 		for i in range(len(deployDict.get('hostname'))):
-			command = 'bgcommander '+ deployDict.get('addr')[i] + ' ' +nfs_path+'/'+deployDict.get('hostname')[i] + 'rw'
+			command = './bgcommander '+ deployDict.get('addr')[i] + ' ' +exportRootPath+'/'+deployDict.get('zone_id')+'/nodes/'+deployDict.get('hostname')[i] + ',rw'
 			print(command)
 #			os.system(command) 	
 
 def main():
 	boot_up_by_images(deployDictSample)
-#	zone_id='zone001'
-#	myfile = open('/etc/exports','a+r')
-#	print(myfile.read().find(zone_id))
-#	if (myfile.read().find(zone_id)):
-#                print(zone_id + ' entry already existed in the exports file')
+
 if __name__ == '__main__':
 	main()
 
