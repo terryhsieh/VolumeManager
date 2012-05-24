ce615463dd8b95c4855d8df4caf1d59396937a15
terry.hsieh <terry.hsieh@delta.com.tw>
2012-05-24 14:41:04 +0800

    modify export and bgcommander function

--- BootVolumeUtil.py
+++ BootVolumeUtil.py
@@ -7,6 +7,7 @@ from shutil import copytree, rmtree
 
 StorageMountPoint="/templateVol"
 ExportPath="192.168.100.10:/nfsroot_test"
+my_ip = '192.168.100.101'
 
 
 #check if mount point exist
@@ -37,7 +38,7 @@ def boot_up_by_images(deployDict):
 			copytree(source_dir,dest_dir)
 			
 			modify_exports_file(deployDict.get('zone_id'))
-			call_bgcommander()
+			call_bgcommander(deployDict)
 		else:
 			print('copy...')
 			source_dir = StorageMountPoint+'/'+deployDict.get('volume')
@@ -45,6 +46,7 @@ def boot_up_by_images(deployDict):
                         copytree(source_dir,dest_dir)
 			
 			modify_exports_file(deployDict.get('zone_id'))	
+			call_bgcommander(deployDict)
 	else:
 		print('volume_readonly is False')
 		if ( os.path.isdir(StorageMountPoint+'/'+deployDict.get('zone_id'))):
@@ -76,28 +78,37 @@ def listImages():
 # The export entry based on zone_id, ex: one zone has one export entry 
 def modify_exports_file(zone_id):
 	print(zone_id)
-	file = open('/etc/exports','a+r')
-	entry = StorageMountPoint +'/' + zone_id +'\t' + '*(rw,async,no_root_squash)'+'\n'
-	if (file.read().find(zone_id)):
-		print(zone_id + 'entry already existed in the exports file')
+	myfile = open('/etc/exports','a+r')
+	entry = StorageMountPoint +'/' + zone_id +'\t' + '*(rw,async,no_root_squash,no_subtree_check)'+'\n'
+	if (myfile.read().find(zone_id) > 0):
+		print(zone_id + ' entry already existed in the exports file')
 	else:
-		file.write(entry)
-	file.close()
+		myfile.write(entry)
+	myfile.close()
 	
 	os.system('exportfs -rv')
 
-def call_bgcommander(client_ip, nfs_path, volume_readonly):
-	if (volume_readyonly == True):
-		#os.system('bgcommander '+ client_ip +' '+nfs_path + ' ' + 'ro')
-		print('bgcommander '+ client_ip +' '+nfs_path + ' ' + 'ro')
+def call_bgcommander(deployDict):
+	if (deployDict.get('volume_readyonly') == True):
+		nfs_path = StorageMountPoint+'/'+deployDict.get('zone_id')
+		for t_addr in deployDict.get('addr'):
+			command = 'bgcommander ' + ip + ' ' + nfs_path + 'ro'
+			print(command)
+#			os.system(command)
 	else:
-		#os.system('bgcommander '+ client_ip + ' '+ nfs_path + ' ' + 'rw')
-		print('bgcommander '+ client_ip + ' '+ nfs_path + ' ' + 'rw')
+		nfs_path = StorageMountPoint+'/'+deployDict.get('zone_id')
+		for i in range(len(deployDict.get('hostname'))):
+			command = 'bgcommander '+ deployDict.get('addr')[i] + ' ' +nfs_path+'/'+deployDict.get('hostname')[i] + 'rw'
+			print(command)
+#			os.system(command) 	
 
 def main():
-#	boot_up_by_images(deployDictSample)
-	volume_name = StorageMountPoint+'/'+deployDictSample.get('zone_id')
-	modify_exports_file(volume_name)
+	boot_up_by_images(deployDictSample)
+#	zone_id='zone001'
+#	myfile = open('/etc/exports','a+r')
+#	print(myfile.read().find(zone_id))
+#	if (myfile.read().find(zone_id)):
+#                print(zone_id + ' entry already existed in the exports file')
 if __name__ == '__main__':
 	main()
 
