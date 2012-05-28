bf30b1955a11c10134905ef62a5d013b1f4fc5c2
terry.hsieh <terry.hsieh@delta.com.tw>
2012-05-28 11:30:47 +0800

    change parameter format

--- BootVolumeUtil.py
+++ BootVolumeUtil.py
@@ -17,67 +17,94 @@ hdlr.setFormatter(formatter)
 logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
-deployDictSample = {	'zone_id':'zone001',
-			'hostname':['host1','host2'],
-			'addr':['192.168.100.30','192.168.100.31'],
-			'kernel':'vmlinuz-3.2.0-23-generic',
-			'ramdisk':'initrd.img-3.2.0-23-generic',
-			'volume':'vol_openstack',
-			'volume_readonly':False}
+deployDictSample = {    'zone_id':'zone001',
 
-# check if /templateStor exist 
-if ( not os.path.isdir(templateStor)):
-	logger.error("The tempate storage folder does not exist!, check if the iSCSI connection to backend storage correctly")
-	exit 0
+                        'host_list':[
+                                        {'hostname':'host1',
+                                         'ip':'192.168.100.31',
+                                         'kernel':'vmlinuz-3.2.0-23-generic',
+                                         'ramdisk':'initrd.img-3.2.0-23-generic',
+                                         'volume':'vol_openstack',
+                                         'read_only':False},
 
-def boot_up_by_images(deployDict):
+                                        {'hostname':'host2',
+                                         'ip':'192.168.100.32',
+                                         'kernel':'vmlinuz-3.2.0-23-generic',
+                                         'ramdisk':'initrd.img-3.2.0-23-generic',
+                                         'volume':'vol_openstack',
+                                         'read_only':True},
+
+                                        {'hostname':'host3',
+                                         'ip':'192.168.100.33',
+                                         'kernel':'vmlinuz-3.2.0-23-generic',
+                                         'ramdisk':'initrd.img-3.2.0-23-generic',
+                                         'volume':'vol_openstack',
+                                         'read_only':True}
+                                    ]
+
+		   }
+
+
+def boot_host(deployDict):
 	
-	logger.debug('input parameter: '+deployDict)
+	check_current_status(deployDict)
 	
-	if ( os.path.isdir(templateStor + '/' + deployDict.get('zone_id'))):
-		logger.info('The zone id: ' + deployDict.get('zone_id') + ' already existed in the template Storage')
-		logger.info('Delete zone id: '+deployDict.get('zone_id'))
-		rmtree(templateStor + '/' + deployDict.get('zone_id'))
-		logger.info('zone_id: ' +deployDict.get('zone_id')+' was deleted')
+	create_volume(deployDict)
 	
-	if deployDict.get("volume_readonly") == True:
-		logger.info('volume_readonly is True')
+	append_to_exports_file(deployDict)
+	
+	call_bgcommander(deployDict)	
 			
-		source_dir = templateStor+'/'+deployDict.get('volume')
-		dest_dir = templateStor+'/'+deployDict.get('zone_id')
-		copytree(source_dir,dest_dir)
-		logger.info('volume '+deployDict.get('volume')+'has already copy to '+dest_dir)		
 
-		modify_exports_file(deployDict)
-		call_bgcommander(deployDict)
+def check_current_status():
 
-	else:
-		print('volume_readonly is False')
+	if ( not os.path.isdir(templateStor)):
+        	logger.error("The tempate storage folder does not exist!, check if the iSCSI connection to backend storage correctly")
+		logger.debug('Input parameter: '+str(deployDict))
 
-		source_dir = templateStor+'/'+deployDict.get('volume')
-		for node in deployDict.get('hostname'):
-			dest_dir = templateStor+'/'+deployDict.get('zone_id') + '/nodes/' + node
-			copytree(source_dir, dest_dir)
-			modify_exports_file(deployDict)
-		call_bgcommander(deployDict)
+        # delete if folder named zone_id exist
+        if ( os.path.isdir(templateStor + '/' + deployDict.get('zone_id'))):
+                logger.info('The zone id: ' + deployDict.get('zone_id') + ' already existed in the template Storage')
+                logger.info('Delete zone id: '+deployDict.get('zone_id'))
+                rmtree(templateStor + '/' + deployDict.get('zone_id'))
+                logger.info('zone_id: ' +deployDict.get('zone_id')+' was deleted')
 
-def reboot(hostname, addr):
-	return True
 
-def shutdown(hostname, addr):
-	return True
+def create_volume(deployDict):
 
-def listImages():
-	pass
+	for host in deployDict.get('host_list'):
+		if host.get('read_only'): #create one ro volume
+			print ('ro '+str(host.get('read_only')))
+			print ('check folder exist?'+str(os.path.isdir(templateStor + '/' + deployDict.get('zone_id')+'/'+'ro')))
+			if ( not  os.path.isdir(templateStor + '/' + deployDict.get('zone_id')+'/'+'ro')):
+				source_dir = templateStor+'/'+host.get('volume')
+               			dest_dir = templateStor+'/'+deployDict.get('zone_id')+'/'+'ro'
+                		copytree(source_dir,dest_dir)
+                		logger.info('volume '+host.get('volume')+'has already copy to '+dest_dir)
+			
+		else: #create rw volume for each host
+			print ('rw '+str(host.get('read_only')))
+			source_dir = templateStor+'/'+host.get('volume')
+			dest_dir = templateStor+'/'+deployDict.get('zone_id') + '/rw/' + host.get('hostname')
+			copytree(source_dir,dest_dir)
+
 
 # The export entry based on zone_id, ex: one zone has one export entry 
-def modify_exports_file(deployDict):
-	myfile = open('/etc/exports','a+r')
-	if (deployDict.get('volume_readonly') == True):
-		entry = templateStor +'/' + deployDict.get('zone_id') +'\t' + '*(ro,async,no_root_squash,no_subtree_check)'+'\n'
-	else:
-		entry = templateStor +'/' + deployDict.get('zone_id') +'\t' + '*(rw,async,no_root_squash,no_subtree_check)'+'\n'
+def append_to_exports_file(deployDict):
 
+	exports_file = open('/etc/exports','a+r')
+	flag_ro = False
+	flag_rw = False
+    
+	for host in deployDict.get('host_list'):
+		if (host.get('read_only') == True):
+			flag_ro = True
+	#		entry = templateStor +'/' + deployDict.get('zone_id') +'\t' + '*(ro,async,no_root_squash,no_subtree_check)'+'\n'
+		if (host.get('read_only') == False)
+			flag_rw = True
+	#		entry = templateStor +'/' + deployDict.get('zone_id') +'\t' + '*(rw,async,no_root_squash,no_subtree_check)'+'\n'
+
+	
 
 	if (myfile.read().find(deployDict.get('zone_id')) > 0):
 		logger.info(' The zone id: ' + deployDict.get('zone_id') + ' already exist in the exports file')
@@ -87,6 +114,7 @@ def modify_exports_file(deployDict):
 
 	myfile.close()
 	os.system('exportfs -rv')
+
 
 def call_bgcommander(deployDict):
 	if (deployDict.get('volume_readonly') == True):
@@ -102,8 +130,22 @@ def call_bgcommander(deployDict):
 			logger.info(command)
 #			os.system(command) 	
 
+
+def reboot(hostname, addr):
+	return True
+
+
+def shutdown(hostname, addr):
+	return True
+
+
+def listImages():
+	pass
+
+
 def main():
-	boot_up_by_images(deployDictSample)
+	boot_host(deployDictSample)
+
 
 if __name__ == '__main__':
 	main()
